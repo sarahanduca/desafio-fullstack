d0c8310fa256a75ea497051d476274ac58730683
sarahanduca <anducasarah@gmail.com>
2024-06-13 19:46:46 -0300

    fix: add migrate into api build and remove from docker compose

--- desafio-fullstack-api/knexfile.ts
+++ desafio-fullstack-api/knexfile.ts
@@ -1,17 +1,16 @@
-import type { Knex } from 'knex';
 import { configDotenv } from 'dotenv';
 
 configDotenv({ path: './.env' });
 
-const config: { [key: string]: Knex.Config } = {
+module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      database: process.env.POSTGRES_DB,
-      user: process.env.POSTGRES_USER,
-      port: parseInt(process.env.POSTGRES_PORT || ''),
-      password: process.env.POSTGRES_PASSWORD,
-      host: process.env.POSTGRES_HOST,
+      database: process.env.POSTGRES_DB || 'desafiofullstack',
+      user: process.env.POSTGRES_USER || 'postgres',
+      port: parseInt(process.env.POSTGRES_PORT || '5432'),
+      password: process.env.POSTGRES_PASSWORD || 'desafio',
+      host: process.env.POSTGRES_HOST || 'postgres',
     },
     migrations: {
       tableName: 'migrations',
@@ -23,24 +22,22 @@ const config: { [key: string]: Knex.Config } = {
     },
   },
 
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: process.env.POSTGRES_DB,
-      user: process.env.POSTGRES_USER,
-      port: parseInt(process.env.POSTGRES_PORT || ''),
-      password: process.env.POSTGRES_PASSWORD,
-      host: process.env.POSTGRES_HOST,
-    },
-    migrations: {
-      tableName: 'migrations',
-      directory: './scripts/migrations',
-      extension: 'ts',
-    },
-    seeds: {
-      directory: './scripts/seeds',
-    },
-  },
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: process.env.POSTGRES_DB,
+  //     user: process.env.POSTGRES_USER,
+  //     port: parseInt(process.env.POSTGRES_PORT || ''),
+  //     password: process.env.POSTGRES_PASSWORD,
+  //     host: process.env.POSTGRES_HOST,
+  //   },
+  //   migrations: {
+  //     tableName: 'migrations',
+  //     directory: './scripts/migrations',
+  //     extension: 'ts',
+  //   },
+  //   seeds: {
+  //     directory: './scripts/seeds',
+  //   },
+  // },
 };
-
-module.exports = config;
--- desafio-fullstack-api/src/main.ts
+++ desafio-fullstack-api/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import Knex from 'knex';
 async function bootstrap() {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const knexConfig = require('../knexfile');
+  const knex = Knex(knexConfig.development);
+  await knex.migrate.latest();
+
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
   await app.listen(process.env.API_PORT);
 }
 bootstrap();
